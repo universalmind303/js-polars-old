5d31b39f1108d8ff0e3ed5fa75c0c2bd8d9b89f6
universalmind303 <universalmind.candy@gmail.com>
2022-04-25 10:32:35 -0500

    add parquet

--- src/dataframe.rs
+++ src/dataframe.rs
@@ -118,17 +118,17 @@ impl JsDataFrame {
     pub fn as_single_chunk_par(&mut self) -> JsDataFrame {
         self.df.as_single_chunk_par().clone().into()
     }
-    // pub fn read_parquet(buf: &[u8]) -> JsResult<JsDataFrame> {
-    //     let reader = Cursor::new(buf);
-    //     let out = ParquetReader::new(reader)
-    //         .with_projection(None)
-    //         .with_columns(None)
-    //         .with_n_rows(None)
-    //         .with_row_count(None)
-    //         .finish()
-    //         .map_err(|e| JsPolarsErr::Other(format!("{:?}", e)))?;
-    //     Ok(out.into())
-    // }
+    pub fn read_parquet(buf: &[u8]) -> JsResult<JsDataFrame> {
+        let reader = Cursor::new(buf);
+        let out = ParquetReader::new(reader)
+            .with_projection(None)
+            .with_columns(None)
+            .with_n_rows(None)
+            .with_row_count(None)
+            .finish()
+            .map_err(|e| JsPolarsErr::Other(format!("{:?}", e)))?;
+        Ok(out.into())
+    }
     pub fn read_json(buf: &[u8]) -> JsResult<JsDataFrame> {
         let reader = Cursor::new(buf);
         let out = JsonReader::new(reader)
