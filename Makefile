d135df93d189b080beaa0a05533a48ffca58a916
universalmind303 <universalmind.candy@gmail.com>
2022-04-24 22:28:11 -0500

    commit msg

--- Makefile
+++ Makefile
@@ -2,6 +2,8 @@
 build-dev:
 	RUSTFLAGS='-C target-feature=+atomics,+bulk-memory,+mutable-globals'; wasm-pack build --dev --target nodejs -d pkg/node
 build-web:
-	RUSTFLAGS='-C target-feature=+atomics,+bulk-memory,+mutable-globals';  wasm-pack build --dev --target web -d pkg/browser
+	RUSTFLAGS='-C target-feature=+atomics,+bulk-memory,+mutable-globals' \
+  	cargo build --target wasm32-unknown-unknown -Z build-std=std,panic_abort
+	yarn webpack
 build-prod:
 	wasm-pack build --target nodejs 
--- worker.js
+++ worker.js
@@ -1,25 +1,31 @@
 
 import * as Comlink from 'comlink';
 import * as pl from './pkg/polars.js'
-
+import fs from "fs"
 self.onmessage = async event => {
   await pl.default()
   await pl.init_hooks()
-  await pl.initThreadPool(2);
+  await pl.initThreadPool(navigator.hardwareConcurrency);
   console.log(pl)
   self.console.log("from worker")
-  const randomArr = () => Array.from({length: 1_000_000}, () => Math.round((Math.random() * 100)));
-  const columns = Array.from({length: 100}, (i) => {
+  const randomArr = () => Array.from({length: 10_000}, () => Math.round((Math.random() * 100 % 100)));
+  const columns = Array.from({length: 10}, (_, i) => {
     return pl.Series.new_f64(`col_${i}`, randomArr())
-
   })
-  const s1 = pl.Series.new_f64("a", randomArr())
-  const s2 = pl.Series.new_f64("b", randomArr())
-  let df = pl.DataFrame.read_columns(columns[Symbol.iterator]())
-  const s3 = pl.Series.new_f64("c", randomArr())
+  
+  const buf = fs.readFileSync("./examples/100k.json")
+  console.log(buf)
+  // const s1 = pl.Series.new_f64("a", randomArr())
+  // const s2 = pl.Series.new_f64("b", randomArr())
+  let df = pl.DataFrame.read_json(columns[Symbol.iterator]())
+  // const s3 = pl.Series.new_f64("c", randomArr())
+  console.profile("unique")
+  console.log(df.as_single_chunk_par().as_str())
+  console.profileEnd("unique")
 
-  df = df.add(s3)
-  console.log(df.unique(false, ['a'], "first").as_str())
+  console.profile("as_single_ptr")
+  console.log(df.as_)
+  console.profileEnd("as_single_ptr")
 };
 
 
