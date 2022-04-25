2c6c846118f5df84b0bd834a17ac9bc1a0a434e7
universalmind303 <universalmind.candy@gmail.com>
2022-04-24 22:32:16 -0500

    commit msg

--- worker.js
+++ worker.js
@@ -1,7 +1,6 @@
 
 import * as Comlink from 'comlink';
 import * as pl from './pkg/polars.js'
-import fs from "fs"
 self.onmessage = async event => {
   await pl.default()
   await pl.init_hooks()
@@ -12,20 +11,13 @@ self.onmessage = async event => {
   const columns = Array.from({length: 10}, (_, i) => {
     return pl.Series.new_f64(`col_${i}`, randomArr())
   })
-  
-  const buf = fs.readFileSync("./examples/100k.json")
-  console.log(buf)
-  // const s1 = pl.Series.new_f64("a", randomArr())
-  // const s2 = pl.Series.new_f64("b", randomArr())
-  let df = pl.DataFrame.read_json(columns[Symbol.iterator]())
-  // const s3 = pl.Series.new_f64("c", randomArr())
-  console.profile("unique")
-  console.log(df.as_single_chunk_par().as_str())
-  console.profileEnd("unique")
+  const buf = (await fetch("https://github.com/universalmind303/js-polars/blob/js-polars-try-again/examples/100k.json")).arrayBuffer()
 
-  console.profile("as_single_ptr")
-  console.log(df.as_)
-  console.profileEnd("as_single_ptr")
+  console.profile("read_json")
+  let df = pl.DataFrame.read_json(buf)
+  console.log(df.as_str())
+  console.profileEnd("read_json")
+
 };
 
 
